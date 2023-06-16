7624f2c9246b8970856fb0f8df9b75a29f674d07
Violet J M <VioletJewel1729@gmail.com>
2023-06-16 15:21:34 -0400

    fix :Sterm bugs and improve
    - no more freezes!
        - had to use w:_term_ins_ instead of b:...
    - C-w . => literal C-w
    - C-w " => insert register
    - fix count

--- plugin/term.lua
+++ plugin/term.lua
@@ -1,57 +1,79 @@
 -- Author: Violet
--- Last Change: 18 January 2023
+-- Last Change: 16 June 2023
+
+-- Overview: basically remove barriers for speedier terminal use.
+-- tl;dr:
+--  :Sterminal => open :terminal in horizontal split
+--  :vertical Sterminal => open :term in vertical split
+--  :term => :Sterm (hack using :cabbrev - only "term" at beg of cmdline)
+--  :vterm => :vert Sterm (hack using :cabbrev)
+--  t_CTRL-W acts like vim
+--    - <c-w>: => issue command
+--    - <c-w>[count]"{reg} => insert register
+--    - <c-w>[count]gt <c-w>[count]gT => tab page nav
+--    - win nav/resize => eg, <c-w>h <c-w>10_ <c-w>= etc
+--    - <c-w>. => literal <c-w>
+--  auto-insert-mode when terminal opened
+--  auto close terminal if no error and shell is user's &shell
+--  nicer buffer name for :term &statusline
 
 local map = require'utils'.map
 local au = require'utils'.buildaugroup('VioletTerm')
 local cmd = vim.api.nvim_create_user_command
 
--- Note: do not mess with or interchange vim.cmd'insert' with nvim_feedkeys()
---       or vice versa,
+-- Note: do not change/switch startinsert w nvim_feedkeys() or vice versa,
 --       and also do not mess with nvim_feedkeys() modes
---       ... unless you want neovim to lock up in tmux
+--       ... unless you want neovim to lock up in tmux ...
+--       and finally be sure to use w: and not b:
+--       tl;dr if you change anything, beware that neovim will probably freeze 
 
--- treat term <c-w> like vim: allow <c-w>:, window navigation, etc
+local N = vim.api.nvim_replace_termcodes('<c-bslash><c-n>', true, true, true)
+
+-- treat term <c-w> like vim
 map{ '<c-w>', function()
   local wid = vim.api.nvim_get_current_win()
-  local count = {}
+  local tcount = {}
   local ch = vim.fn.getcharstr()
+  if ch == '.' then -- t_ctrl-w . => literal ctrl-w
+    local ctrlw = vim.api.nvim_replace_termcodes('<c-w>', true, true, true)
+    return vim.api.nvim_feedkeys(ctrlw, 'nt', false)
+  end
   if ch ~= '0' then
-    while ch:match('^%d$') do
-      count[#count+1] = ch
+    while ch:match('^%d$') do -- count: /[1-9][0-9]*/
+      tcount[#tcount+1] = ch
       ch = vim.fn.getcharstr()
     end
   end
-  count = table.concat(count, '')
-  if ch == 'g' then
+  count = table.concat(tcount, '')
+  if ch == 'g' then -- t_ctrl-w gt, t_ctrl-w gT => :tabnext, :tabprev
     ch = ch..vim.fn.getcharstr()
   end
   local keys
-  if ch == 'N' then
-    keys = vim.api.nvim_replace_termcodes('<c-bslash><c-n>', true, true, true)
-    return vim.api.nvim_feedkeys(keys, 'n', false)
+  if ch == 'N' then -- t_ctrl-w N => ctrl-bslash ctrl-n
+    return vim.api.nvim_feedkeys(N, 'n', false)
   end
+  if ch == '"' then -- t_ctrl-w [count] " {reg} => insert {reg} [count] times
+    ch = vim.fn.getcharstr()
+    return vim.api.nvim_feedkeys(N..count..'"'..ch..'pi', 'nt', false)
+  end
+  vim.api.nvim_win_set_var(wid, '_term_ins_', true)
   keys = vim.api.nvim_replace_termcodes('<c-bslash><c-n>'..count..'<c-w>'..ch, true, true, true)
-  vim.api.nvim_feedkeys(keys, ch == 'N' and 'n' or 'x!', false)
-  if vim.api.nvim_get_current_win() == wid then
-    vim.api.nvim_feedkeys('i', 'nx!', false)
-  else
-    vim.api.nvim_buf_set_var(vim.api.nvim_win_get_buf(wid), '_term_ins_', true)
-  end
+  vim.api.nvim_feedkeys(keys, '', false)
 end, modes='t' }
 
 -- re-enter insert mode when coming back to a terminal when it was left with <c-w> map above
 au{ 'BufWinEnter,WinEnter,CmdlineLeave', function()
-    if vim.o.buftype == 'terminal' and vim.b._term_ins_ then
-      vim.cmd'startinsert'
-      vim.b._term_ins_ = nil
+    if vim.o.buftype == 'terminal' and vim.w._term_ins_ then
+      if vim.fn.mode(1) == 'nt' then
+        vim.cmd'startinsert'
+        vim.w._term_ins_ = false
+      end
     end
   end }
 
--- split terminal command(s)
 cmd('Sterminal', '<mods> split | terminal <args>', { nargs='*' })
 cmd('STerminal', '<mods> split | terminal <args>', { nargs='*' })
 
--- :term and :vterm hacks to open terminals in splits (:ter/:terminal still work "normally")
 vim.cmd[[ cabbrev <expr> term getcmdtype() is ':' && getcmdline() =~# '^term' && getcmdpos() is 5 ? 'Sterm' : 'term' ]]
 vim.cmd[[ cabbrev <expr> vterm getcmdtype() is ':' && getcmdline() =~# '^vterm' && getcmdpos() is 6 ? 'vert Sterm' : 'vterm' ]]
 
@@ -64,14 +86,16 @@ au{ 'TermOpen', function()
       end
       vim.b.shortname = shortname
       vim.wo.statusline = '%{b:shortname}%=[terminal]'
-      vim.cmd'startinsert'
+      if vim.fn.mode(1) ~= 't' then
+        vim.cmd'startinsert'
+      end
     end
   end }
 
--- auto-close response when terminal closed (only if $?=0 and prgm=&shell)
 au{ 'TermClose', function()
-    if vim.o.shell == vim.api.nvim_buf_get_name(0):gsub('%S*:','') and vim.v.event.status == 0 then
-      vim.fn.feedkeys('q', 'nt')
+    if vim.o.shell == vim.api.nvim_buf_get_name(0):gsub('%S*:','')
+      and vim.v.event.status == 0 then
+      vim.fn.feedkeys(' ', 'nt')
     end
   end }
 
